66d50f831c4500f867e1681be9d80f6f3b6f10be
joaoteixeira <johnnie.6@hotmail.com>
2023-03-21 22:33:46 +0000

    finished

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
-
-
     
-
+    @IBAction func KeyPressed(_ sender: UIButton) {
+        
+        playSound(name: sender.currentTitle!)
+        
+        //diminuir luminosidade do botao durante 0.2s
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+ 
+    }
+    
+    
+    func playSound(name: String)
+    {
+        let url = Bundle.main.url(forResource: name, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
 }
 
